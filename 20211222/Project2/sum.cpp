ea15347b9713a4ff3bf9d458ccd2dc46d612f15a
lunasinfographic <newsunnydreams@naver.com>
2021-12-22 17:15:00 +0900

    반복문 소스 연습

---
+++ 20211222/Project2/sum.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	int sum = 0;
+
+	for (int i = 1; i < 101; ++i)
+	{
+
+		 sum=sum + i;
+
+	}
+
+	cout << sum << endl;
+
+	return 0;
+
+}
+
+
